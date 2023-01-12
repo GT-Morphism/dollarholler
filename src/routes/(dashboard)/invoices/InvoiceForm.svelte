68e5050309ac2c52184ab1461b234a389087dfa0
Giuseppe Turturiello <giuseppe-pio@hotmail.de>
2023-01-12 12:26:34 +0100

    Add basic funtionality to add new client

--- src/routes/(dashboard)/invoices/InvoiceForm.svelte
+++ src/routes/(dashboard)/invoices/InvoiceForm.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
+	import { slide } from "svelte/transition";
 	import { v4 as uuid } from "uuid";
+	import { states } from "$lib/utils/states";
 	import Button from "$lib/components/Button.svelte";
 	import TrashIcon from "$lib/components/Icon/TrashIcon.svelte";
 	import LineItemRows from "./LineItemRows.svelte";
@@ -12,6 +14,7 @@
 	};
 
 	let lineItems: LineItem[] = [{ ...blankLineItem }];
+	let isNewClient = false;
 
 	const addLineItem = () => {
 		lineItems = [...lineItems, { ...blankLineItem, id: uuid() }];
@@ -32,15 +35,38 @@
 
 <form class="grid grid-cols-6 gap-x-5">
 	<!-- client -->
-	<div class="field col-span-2">
-		<label for="client">Client</label>
-		<select name="client" id="client">
-			<option value="zeal">ZEAL</option>
-		</select>
-	</div>
-	<div class="field col-span-2 flex items-end gap-x-5">
-		<div class="text-base font-bold leading-[3.5rem] text-monsoon">or</div>
-		<Button style="outline" label="+ Client" isAnimated={false} />
+	<div class="field col-span-4">
+		{#if !isNewClient}
+			<label for="client">Client</label>
+			<div class="flex items-end gap-x-5">
+				<select name="client" id="client">
+					<option value="zeal">ZEAL</option>
+				</select>
+				<div class="text-base font-bold leading-[3.5rem] text-monsoon">or</div>
+				<Button
+					style="outline"
+					label="+ Client"
+					isAnimated={false}
+					on:click={() => {
+						isNewClient = true;
+					}}
+				/>
+			</div>
+		{:else}
+			<label for="newClient">New Client</label>
+			<div class="flex items-end gap-x-5">
+				<input type="text" name="newClient" />
+				<div class="text-base font-bold leading-[3.5rem] text-monsoon">or</div>
+				<Button
+					style="outline"
+					label="Existing Client"
+					isAnimated={false}
+					on:click={() => {
+						isNewClient = false;
+					}}
+				/>
+			</div>
+		{/if}
 	</div>
 
 	<!-- invoice id -->
@@ -48,6 +74,41 @@
 		<label for="id">Invoice ID</label>
 		<input type="number" name="id" />
 	</div>
+
+	<!-- new client -->
+	{#if isNewClient}
+		<div class="field col-span-6 grid gap-x-5" transition:slide>
+			<div class="field col-span-6">
+				<label for="email">Client's Email</label>
+				<input type="email" name="email" id="email" />
+			</div>
+
+			<div class="field col-span-6">
+				<label for="street">Client's Street</label>
+				<input type="text" name="street" id="street" />
+			</div>
+
+			<div class="field col-span-2">
+				<label for="city">Client's City</label>
+				<input type="text" name="city" id="city" />
+			</div>
+
+			<div class="field col-span-2">
+				<label for="state">Client's State</label>
+				<select name="state" id="state">
+					<option />
+					{#each states as state}
+						<option value={state.value}>{state.name}</option>
+					{/each}
+				</select>
+			</div>
+
+			<div class="field col-span-2">
+				<label for="zip">Client's Zip</label>
+				<input type="text" name="zip" id="zip" />
+			</div>
+		</div>
+	{/if}
 
 	<!-- due date -->
 	<div class="field col-span-2">
