86a6487763c4ee035f04eb3ac81c05a3df8f7a07
Giuseppe Turturiello <giuseppe-pio@hotmail.de>
2023-01-17 15:33:38 +0100

    Add animation to search input

--- src/lib/components/Search.svelte
+++ src/lib/components/Search.svelte
@@ -10,5 +10,26 @@
 		placeholder="Search by Keyword"
 		class="search w-full border-b-2 border-dashed border-b-pastelPurple bg-transparent font-sansSerif text-base text-black outline-none focus:border-solid focus:border-b-lavenderIndigo md:w-[14rem] lg:w-72 lg:text-xl"
 	/>
-	<button class="font-sansSerif text-base font-black text-pastelPurple lg:text-xl">Search</button>
+	<button
+		class="pointer-events-none font-sansSerif text-base font-black text-pastelPurple transition-transform duration-[400ms] md:absolute md:left-8 lg:text-xl"
+		>Search</button
+	>
 </div>
+
+<style lang="postcss">
+	@media (min-width: 768px) {
+		::placeholder {
+			color: transparent;
+		}
+	}
+
+	input:not(:placeholder-shown) + button,
+	input:focus + button {
+		@apply pointer-events-auto text-lavenderIndigo md:translate-x-[14rem] lg:translate-x-[18rem];
+	}
+
+	input:focus + button:hover,
+	input + button:focus {
+		@apply text-daisyBush;
+	}
+</style>
