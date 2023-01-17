00618a66b8c3ecb698a332417d5393f3e9535584
Giuseppe Turturiello <giuseppe-pio@hotmail.de>
2023-01-17 15:05:51 +0100

    Add pages regarding auth

---
+++ src/routes/(auth)/forgot-password/+page.svelte
@@ -0,0 +1,12 @@
+<h1 class="auth-heading">Forgot Password</h1>
+
+<form>
+	<div class="field">
+		<label for="email" class="text-goldenFizz">Email Address</label>
+		<input type="email" name="email" placeholder="Your Email" />
+	</div>
+	<button type="submit" class="auth-button"> Send me a Reset Email </button>
+	<a href="/login" class="mt-4 block text-center text-sm text-white underline hover:no-underline"
+		>Ready to login?</a
+	>
+</form>
